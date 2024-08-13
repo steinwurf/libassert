eead87829fb0d80d2c3de18afc94c31a8193bc07
Sofie Finnes Øvrelid <sofie+git@steinwurf.com>
2024-08-13 15:58:03 +0200

    chore: run clang-format

--- test/src/test_libassert.cpp
+++ test/src/test_libassert.cpp
@@ -1,12 +1,9 @@
 #include <gtest/gtest.h>
 #include <libassert/assert.hpp>
 
-TEST(test_libassert, simple)
-{
-    int a = 42;
-    ASSERT(a == 42);
-    ASSERT(a = 21);
-    ASSERT(a == 21);
+TEST(test_libassert, simple) {
+  int a = 42;
+  ASSERT(a == 42);
+  ASSERT(a = 21);
+  ASSERT(a == 21);
 }
-
-
