4d52c8d80ccad8146e7d710fd76fa7908bd38e1e
Mikkel Højlund Larsen <mikkelhojlundgrontmij@gmail.com>
2024-08-21 10:23:42 +0200

    working on it

--- test/src/test_libassert.cpp
+++ test/src/test_libassert.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 #include <libassert/assert.hpp>
 
+#ifdef _WIN32
+__declspec(dllimport) void __stdcall OutputDebugStringA(
+    const char *lpOutputString);
+#endif
+
 TEST(test_libassert, simple) {
   int a = 42;
   ASSERT(a == 42);
